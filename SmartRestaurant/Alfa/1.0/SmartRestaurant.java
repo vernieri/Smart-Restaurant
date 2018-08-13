cd1ea89d1b58ce2684d83e07c56bad311ef6fec0
Vernieri <31966345+vernieri@users.noreply.github.com>
2018-08-13 17:28:12 -0300

    Update SmartRestaurant.java

--- SmartRestaurant/Alfa/1.0/SmartRestaurant.java
+++ SmartRestaurant/Alfa/1.0/SmartRestaurant.java
@@ -1,1 +1,10 @@
 package smartrestaurant;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
